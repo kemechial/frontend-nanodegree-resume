6a34a100f1c3247cc5cd7df9292e21ccf5b8450e
Kaan <Kaan@DeveloperK>
2015-01-09 15:25:32 +0200

    Some modifications...

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,18 @@
+/*
 $("#main").append("Developer K");
+
+var age=32;
+var awesomeThoughts="I am K and I am awesome!";
+console.log(awesomeThoughts);
+var email="ken@udacity.com";
+var newEmail=email.replace('udacity', 'gmail'); 
+console.log(email);
+console.log(newEmail);
+var funThoughts=awesomeThoughts.replace("awesome", "fun");
+$("#main").append("<br>"+funThoughts);
+*/
+var firstName="Developer K";
+var formattedName=HTMLheaderName.replace("%data%", firstName);
+var formattedRole=HTMLheaderRole.replace("%data%", "Web Developer");
+$("#main").prepend(formattedRole).prepend(formattedName);
+var testArray=['test1','test2'];
